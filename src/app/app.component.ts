905037f88343a8929ea4724c4b87dc2f9030f4b8
Shetty <rajat.shetty@cgi.com>
2018-01-01 12:25:42 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
+  body: string;
+  private http: Http;
+  constructor(http:Http){
+    this.http = http;
+  }
+  ngOnInit() {
+    this.getAllTodos().subscribe();
+  }
+
+  public getAllTodos(): Observable<any> {
+    debugger;
+    return this.http
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .map(response => {
+        const todos = response.json();
+        this.body = todos.body;
+        console.log(todos.body);
+      });
+  }
+  
 }
